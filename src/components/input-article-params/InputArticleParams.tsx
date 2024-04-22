23823533c699bde16ebc3ec60318d768e2cb748b
Aleksey Orekhov <aleoreh@gmail.com>
2024-04-22 10:16:48 +0300

    Добавляет на форму выбор семейства шрифтов

--- src/components/input-article-params/InputArticleParams.tsx
+++ src/components/input-article-params/InputArticleParams.tsx
@@ -1,4 +1,6 @@
+import { OptionType, fontFamilyOptions } from 'src/constants/articleProps';
 import { ArticleStyle } from 'src/types';
+import { Select } from '../select';
 import { Text } from '../text';
 
 type InputArticleParams = (props: {
@@ -6,12 +8,27 @@ type InputArticleParams = (props: {
 	setInput: (articleStyle: ArticleStyle) => void;
 }) => JSX.Element;
 
-export const InputArticleParams: InputArticleParams = () => {
+export const InputArticleParams: InputArticleParams = ({ input, setInput }) => {
+	// семейство шрифтов
+	const defaultFont =
+		fontFamilyOptions.find((x) => x.value === input['--font-family']) ||
+		fontFamilyOptions[0];
+
+	const onFontSelected = (font: OptionType) => {
+		setInput({ ...input, '--font-family': font.value });
+	};
+
 	return (
 		<>
 			<Text size={31} weight={800} uppercase>
 				Задайте параметры
 			</Text>
+			<Select
+				title='Шрифт'
+				options={fontFamilyOptions}
+				selected={defaultFont}
+				onChange={onFontSelected}
+			/>
 		</>
 	);
 };
