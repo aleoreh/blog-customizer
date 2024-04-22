a46a94a8d3c98b65c58c77538d9349856ff69b04
Aleksey Orekhov <aleoreh@gmail.com>
2024-04-22 13:48:29 +0300

    Устанавливает поведение при нажатии на сброс

--- src/index.tsx
+++ src/index.tsx
@@ -41,6 +41,8 @@ const App = () => {
 
 	const resetArticleParamsForm = () => {
 		setInputArticleStyle(defaultStyle);
+		setArticeStyle(defaultStyle);
+		setArticleParamsFormOpened(false);
 	};
 
 	return (
