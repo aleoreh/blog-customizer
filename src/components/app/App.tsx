0a53c2c5e899a4de92b08c4911a53f3158188fea
Aleksey Orekhov <aleoreh@gmail.com>
2024-04-23 08:42:43 +0300

    Выносит App в отдельный модуль
    -
    Обычно корневой index.tsx держат максимально компактным.
    Стоит вынести компонент App в отдельный файл в папке
    components/app/App.tsx.

---
+++ src/components/app/App.tsx
@@ -0,0 +1,65 @@
+import clsx from 'clsx';
+import { CSSProperties, useState } from 'react';
+
+import styles from './App.module.scss';
+
+import {
+	ArticleStateType,
+	defaultArticleState,
+} from 'src/constants/articleProps';
+import { Article } from '../article';
+import { ArticleParamsForm } from '../article-params-form';
+import { InputArticleParams } from '../input-article-params';
+
+const articleStateToStyle = (articleState: ArticleStateType) => ({
+	'--font-family': articleState.fontFamilyOption.value,
+	'--font-size': articleState.fontSizeOption.value,
+	'--font-color': articleState.fontColor.value,
+	'--container-width': articleState.contentWidth.value,
+	'--bg-color': articleState.backgroundColor.value,
+});
+
+export const App = () => {
+	// стиль, который применяется к статье
+	const [articleStyleState, setArticleStyleState] =
+		useState<ArticleStateType>(defaultArticleState);
+
+	// стиль, который вводится пользователем
+	const [inputArticleStyleState, setInputArticleStyleState] =
+		useState<ArticleStateType>(defaultArticleState);
+
+	// видимость формы параметров
+	const [articleParamsFormOpened, setArticleParamsFormOpened] =
+		useState<boolean>(false);
+
+	// при утверждении формы ввода параметров устанавливаем их в стили статьи
+	const submitArticleParamsForm = () => {
+		setArticleStyleState(inputArticleStyleState);
+		setArticleParamsFormOpened(false);
+	};
+
+	const resetArticleParamsForm = () => {
+		setInputArticleStyleState(defaultArticleState);
+		setArticleStyleState(defaultArticleState);
+		setArticleParamsFormOpened(false);
+	};
+
+	return (
+		<div
+			className={clsx(styles.main)}
+			style={articleStateToStyle(articleStyleState) as CSSProperties}>
+			<ArticleParamsForm
+				open={articleParamsFormOpened}
+				setOpen={setArticleParamsFormOpened}
+				submit={submitArticleParamsForm}
+				reset={resetArticleParamsForm}>
+				{/* поля ввода параметров передаются как "дети" формы */}
+				<InputArticleParams
+					input={inputArticleStyleState}
+					setInput={setInputArticleStyleState}
+				/>
+			</ArticleParamsForm>
+			<Article />
+		</div>
+	);
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,72 +1,12 @@
-import clsx from 'clsx';
-import { CSSProperties, StrictMode, useState } from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { Article } from './components/article/Article';
-import { InputArticleParams } from './components/input-article-params';
-import {
-	ArticleStateType,
-	defaultArticleState,
-} from './constants/articleProps';
-import styles from './styles/index.module.scss';
 import './styles/index.scss';
+
+import { App } from './components/app/App';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
-
-const articleStateToStyle = (articleState: ArticleStateType) => ({
-	'--font-family': articleState.fontFamilyOption.value,
-	'--font-size': articleState.fontSizeOption.value,
-	'--font-color': articleState.fontColor.value,
-	'--container-width': articleState.contentWidth.value,
-	'--bg-color': articleState.backgroundColor.value,
-});
-
-const App = () => {
-	// стиль, который применяется к статье
-	const [articleStyleState, setArticleStyleState] =
-		useState<ArticleStateType>(defaultArticleState);
-
-	// стиль, который вводится пользователем
-	const [inputArticleStyleState, setInputArticleStyleState] =
-		useState<ArticleStateType>(defaultArticleState);
-
-	// видимость формы параметров
-	const [articleParamsFormOpened, setArticleParamsFormOpened] =
-		useState<boolean>(false);
-
-	// при утверждении формы ввода параметров устанавливаем их в стили статьи
-	const submitArticleParamsForm = () => {
-		setArticleStyleState(inputArticleStyleState);
-		setArticleParamsFormOpened(false);
-	};
-
-	const resetArticleParamsForm = () => {
-		setInputArticleStyleState(defaultArticleState);
-		setArticleStyleState(defaultArticleState);
-		setArticleParamsFormOpened(false);
-	};
-
-	return (
-		<div
-			className={clsx(styles.main)}
-			style={articleStateToStyle(articleStyleState) as CSSProperties}>
-			<ArticleParamsForm
-				open={articleParamsFormOpened}
-				setOpen={setArticleParamsFormOpened}
-				submit={submitArticleParamsForm}
-				reset={resetArticleParamsForm}>
-				{/* поля ввода параметров передаются как "дети" формы */}
-				<InputArticleParams
-					input={inputArticleStyleState}
-					setInput={setInputArticleStyleState}
-				/>
-			</ArticleParamsForm>
-			<Article />
-		</div>
-	);
-};
 
 root.render(
 	<StrictMode>
